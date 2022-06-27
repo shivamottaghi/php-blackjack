4391215d9efa3e2f44c7e3f5d84345d8e4a38174
shivamtg <shivamtg1376@gmail.com>
2022-06-27 11:02:52 +0200

    add index.php

--- index.php
+++ index.php
@@ -1,1 +1,13 @@
 <?php
+declare(strict_types=1);
+
+require 'mySrc/Suit.php';
+require 'mySrc/Card.php';
+require 'mySrc/Deck.php';
+
+$deck = new Deck();
+$deck->shuffle();
+foreach($deck->getCards() AS $card) {
+    echo $card->getUnicodeCharacter(true);
+    echo '<br>';
+}
