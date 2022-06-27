fa0164d0bd24887c3e0c2f9830322866fbcb9578
shivamtg <shivamtg1376@gmail.com>
2022-06-27 11:32:43 +0200

    add class Blackjack

---
+++ mySrc/Blackjack.php
@@ -0,0 +1,47 @@
+<?php
+
+class Blackjack
+{
+    private Player $player;
+    private Player $dealer;
+    private Deck $deck;
+
+    /**
+     *
+     */
+    public function __construct()
+    {
+        $this->deck = new Deck();
+        $this->deck->shuffle();
+        $this->player = new Player($this->deck);
+        $this->dealer = new Player($this->deck);
+    }
+
+
+    /**
+     * @return Player
+     */
+    public function getPlayer(): Player
+    {
+        return $this->player;
+    }
+
+    /**
+     * @return Player
+     */
+    public function getDealer(): Player
+    {
+        return $this->dealer;
+    }
+
+    /**
+     * @return Deck
+     */
+    public function getDeck(): Deck
+    {
+        return $this->deck;
+    }
+
+
+
+}
