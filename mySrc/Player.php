20467b5d46545142e0e84ee3ea1c90f4b8a4ad23
shivamtg <shivamtg1376@gmail.com>
2022-06-27 11:11:24 +0200

    add empty methods to player

--- mySrc/Player.php
+++ mySrc/Player.php
@@ -2,5 +2,22 @@
 
 class Player
 {
+    private $cards = [];
+    private bool $lost = false;
 
+    public function hit(){
+
+    }
+
+    public function surrender(){
+
+    }
+
+    public function getScore(){
+
+    }
+
+    public function hasLost(){
+
+    }
 }
