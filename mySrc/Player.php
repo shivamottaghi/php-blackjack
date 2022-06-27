1c5836ccf54dae523f52ce98cf497c587de189e3
shivamtg <shivamtg1376@gmail.com>
2022-06-27 11:50:06 +0200

    add Player constructor

--- mySrc/Player.php
+++ mySrc/Player.php
@@ -5,6 +5,15 @@ class Player
     private $cards = [];
     private bool $lost = false;
 
+    /**
+     * @param Deck $deck
+     */
+    public function __construct(Deck $deck)
+    {
+        $this->cards = $deck->getCards();
+        $deck->drawCard();
+    }
+
     public function hit(){
 
     }
